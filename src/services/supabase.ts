b379fc3f168a2b928c27ed1a9325c7347da0bd3b
IsraelArias <israelarifra@gmail.com>
2025-10-29 00:55:59 -0600

    ya jalo otravez en web, pero en movil aun no

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
@@ -17,9 +18,9 @@ if (!supabaseAnonKey) {
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
+    storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
-    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
   },
 });
